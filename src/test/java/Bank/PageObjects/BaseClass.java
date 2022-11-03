4e5e6fed50efb6d494ec107c336bd6657b00bc24
19di32qa <87597749+19di32qa@users.noreply.github.com>
2022-11-03 21:14:25 +0200

    added log4j2, there is bug in old versions it doesn't matter where you put log4j2.xml but know you should place it in main/resourses or add additional file

--- src/test/java/Bank/PageObjects/BaseClass.java
+++ src/test/java/Bank/PageObjects/BaseClass.java
@@ -1,11 +1,15 @@
 package Bank.PageObjects;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class BaseClass {
+    public Logger log = LoggerFactory.getLogger(BaseClass.class);
     public static WebDriver driver;
     public String baseURL = "https://demo.guru99.com/V1/index.php";
     public String userName = "mngr451715";
@@ -17,6 +21,8 @@ public class BaseClass {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Dima\\Desktop\\TestngProject\\src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
+        log.info("webdriver is set");
+
     }
     @AfterClass
     public void tearDown() {
--- src/test/java/Bank/TestCases/TC_0001_Login.java
+++ src/test/java/Bank/TestCases/TC_0001_Login.java
@@ -11,10 +11,12 @@ public class TC_0001_Login extends BaseClass {
     @Test
     public void loginTest() {
         LoginPage lp = new LoginPage(driver);
+        log.info("test");
         lp.homePage(baseURL);
         lp.setUserName(userName);
         lp.setPassword(password);
         lp.submitClick();
         Assert.assertEquals(driver.getTitle(),"GTPL Bank Manager HomePage");
+        log.error("error test");
     }
 }
